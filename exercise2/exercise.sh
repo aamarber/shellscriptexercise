ef14f5990c82dce9ba77288ecbafcea13fb2c88f
melocotoneroh <aamarber@gmail.com>
2023-05-04 16:36:29 +0200

    Exercise working, pending rephrase of the exercise statement

---
+++ exercise2/exercise.sh
@@ -0,0 +1,43 @@
+echo "The password should be at least 8 characters long, contains at least one uppercase letter, one lowercase letter and one digit"
+
+uppercasePattern=[A-Z]
+lowercasePattern=[a-z]
+numberPattern=[0-9]
+
+while [[ $valid != 1 && $again != 'n' ]]
+do
+    read -p "Input the password:" password
+
+    valid=1
+    again=''
+
+    if [[ ! $password =~ $uppercasePattern ]]
+    then
+        echo "The password '$password' does not contain an uppercase letter"
+        valid=0
+    fi
+
+    if [[ ! $password =~ $lowercasePattern ]]
+    then
+        echo "The password '$password' does not contain a lowercase letter"
+        valid=0
+    fi
+
+    if [[ ! $password =~ $numberPattern ]]
+    then
+        echo "The password '$password' does not contain a digit"
+        valid=0
+    fi
+
+    if [[ $valid == 1 ]]
+    then
+        echo 'Valid password!'
+        break
+    fi
+
+    while [[ $again != 'y' && $again != 'n' ]]
+    do
+        echo 'You want to try again? y/n'
+        read again
+    done
+done
